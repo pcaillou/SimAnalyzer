75c47ae06da24c8c7ebec8ca7746f4041a83a916
destannes <alexandre.destannes@u-psud.fr>
2012-06-12 16:21:50 +0200

    ajout de BinObserver

---
+++ NetlogoController/src/statistic/BinObserver.java
@@ -0,0 +1,74 @@
+package statistic;
+
+import org.ujmp.core.Matrix;
+import org.ujmp.core.MatrixFactory;
+import org.ujmp.core.enums.ValueType;
+
+import clustering.event.DataEvent;
+
+public class BinObserver extends StatisticalObserver {
+long step = 1;
+	
+	public static String[] ParamNames={"ListenTo","ObservedBy","ColumnName","intervalStart","intervalWidth","intervalQuantity","","","","","","","","","","","","","",""};
+	public static String[] DefaultValues={"0","1","","0","5","4","","","","","","","","","","","","","",""};
+	
+	/* temporaire le temps d'avoir les vrai constantes de label graph */
+	private String LABEL_VAR = "";
+	private Double INTERVAL_START = 0d;
+	private Double INTERVAL_WIDTH = 0d;
+	private Double INTERVAL_QUANTITY = 0d;
+	
+	public void setParams(String[] paramvals)
+	{
+		try {
+			this.LABEL_VAR=paramvals[2];
+			this.INTERVAL_START = Double.valueOf(paramvals[3]);
+			this.INTERVAL_WIDTH = Double.valueOf(paramvals[4]);
+			this.INTERVAL_QUANTITY = Double.valueOf(paramvals[5]);
+		} catch (NumberFormatException e) {
+			System.err.println("Erreur lors de la recuperation des parametres");
+			e.printStackTrace();
+			}
+	}
+	
+	public BinObserver() {
+		super();
+	}
+	
+	public void newDataAvailable(DataEvent de) throws Exception {
+		Matrix data = de.getData();
+		Matrix result = MatrixFactory.zeros(ValueType.STRING, data.getRowCount(), 1);
+
+		long idColumn = data.getColumnForLabel(LABEL_VAR);
+		Double value = 0d;
+		Integer group = 0;
+		
+		for (int i = 0 ; i < data.getRowCount() ; i++)
+		{
+			value = data.getAsDouble(i,idColumn);
+			value -= INTERVAL_START;
+			if (value % INTERVAL_WIDTH == 0)
+			{
+				value -= INTERVAL_WIDTH/10;
+			}
+			group = (int) (value / INTERVAL_WIDTH) + 1;
+			if (group < 1 && group > INTERVAL_QUANTITY)
+			{
+				group = 1;
+				System.err.println("BinObserver error : value is out of intervals (group set to 1) : " + data.getAsDouble(i,idColumn) + " - " + INTERVAL_START + "->" + (INTERVAL_START + INTERVAL_QUANTITY * INTERVAL_WIDTH) + " ");
+			}
+			result.setAsString("B" + group,i,0);
+		}
+		
+		
+		//data.showGUI();
+		result.showGUI();
+		//pause();
+		
+		/* on envoie le resultat */
+		this.preventListeners(new DataEvent(result, de.getArguments()));
+		
+		step++;
+		
+	}
+}
